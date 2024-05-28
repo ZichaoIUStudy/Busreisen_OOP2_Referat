461b9a7501a06fe82c4c839fe58ab571eedfa8c2
Kevin <kevin.muehl@iu-study.org>
2024-05-28 20:39:11 +0200

    BusseTable angepasst; Table-Klassen erstellt

---
+++ src/main/java/iuinformatik/busreisen/busreisen_oop2_referat/tables/AdressenTable.java
@@ -0,0 +1,8 @@
+package iuinformatik.busreisen.busreisen_oop2_referat.tables;
+
+public class AdressenTable {
+
+    public void createDefaultAdressen() {
+
+    }
+}
---
+++ src/main/java/iuinformatik/busreisen/busreisen_oop2_referat/tables/FahrerTable.java
@@ -0,0 +1,8 @@
+package iuinformatik.busreisen.busreisen_oop2_referat.tables;
+
+public class FahrerTable {
+
+    public void createDefaultFahrer() {
+
+    }
+}
